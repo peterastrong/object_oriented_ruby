aec402f03d4ed896fb58cc87514ec1b43fb02187
Peter Strong <peter.strong@gmail.com>
2017-01-09 19:58:05 -0500

    created three items in hashes

---
+++ store_item.rb
@@ -0,0 +1,4 @@
+item1 = {name: "lego", version: "batman's cave", price: 34.99}
+item1 = {name: "Brio", version: "zoo train", price: 12.55}
+item3 = {:name => "doll", :version => "Dora", :price => 15.99}
+
